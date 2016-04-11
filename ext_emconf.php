3359cbe92cd5eb18f1e6e5c24bfbb577c61615b1
Phat Hoang <phat@pixelant.se>
2016-04-11 10:45:01 +0200

    create extension

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Pixelant Frontend Editing',
+  'description' => '',
+  'category' => '',
+  'version' => '0.0.0',
+  'state' => 'stable',
+  'clearcacheonload' => true,
+  'author' => 'Pixelant',
+  'author_email' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.9.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
