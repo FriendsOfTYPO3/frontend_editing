0e746ca149e7d8a71dc2ff3a4fe94646b80d8e72
Mattias Nilsson <mattias@pixelant.sem>
2016-07-22 07:48:36 +0200

    [TASK] Reset ui config for CKEditor to default

--- Resources/Public/main.js
+++ Resources/Public/main.js
@@ -1,26 +1,18 @@
 (function($, w){
 
 	var editorConfig = {
-		uiColor : '#FF0000'
+
 	};
 
-	$('p').attr('contenteditable', true).ckeditor(editorConfig, function(e){
+	$('h2').attr('contenteditable', true).ckeditor(editorConfig, function(e){
 
 	});
 
-	for(var i in CKEDITOR.instances){
+	for (var i in CKEDITOR.instances){
 		CKEDITOR.instances[i].on('change', function(evt){
 			var editor = evt.editor;
 			console.log(editor.getData());
 		});
 	}
-	
-	/*
-	 CKEDITOR.inline( 'c164',
-	 {
-	 toolbar : 'links',
-	 uiColor : 'yellow'
-	 });
-	 */
 
 })(jQuery, window);
