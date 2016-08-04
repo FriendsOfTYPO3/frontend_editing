2b94e4b7a94f0864d2abaefc2ba0d9aa245303c4
Mattias Nilsson <mattias@pixelant.se>
2016-08-04 14:30:27 +0200

    [TASK] Add npm tasks for running PHP codesniffer and Unit tests

---
+++ Tests/Unit/Controller/JustControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Pixelant\PxaFeediting\Tests\Unit\Controller;
+
+/**
+ * Test case for class Pixelant\PxaFeediting\Controller\JustController.
+ */
+class JustControllerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function anotherUserTest()
+    {
+        $this->assertSame(
+            '',
+            ''
+        );
+    }
+}
