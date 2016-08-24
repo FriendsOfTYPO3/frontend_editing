8a59fb74942b3215889a9b124a58646f87540d39
Mattias Nilsson <mattias@pixelant.se>
2016-08-24 15:14:28 +0200

    [BUGFIX] Fix PHPCGL issues for RequestPreProcessInterface

--- Classes/Utility/RequestPreProcess/RequestPreProcessInterface.php
+++ Classes/Utility/RequestPreProcess/RequestPreProcessInterface.php
@@ -1,18 +1,19 @@
 <?php
 namespace TYPO3\CMS\FrontendEditing\Utility\RequestPreProcess;
 
-interface RequestPreProcessInterface {
-
-	/**
-	 * Preprocess the request
-	 *
-	 * @param array $request save request
-	 * @param boolean $finished
-	 * @param \TYPO3\CMS\FrontendEditing\Controller\SaveController $parentObject
-	 * @return array
-	 */
-	public function preProcess(array &$request, &$finished, \TYPO3\CMS\FrontendEditing\Controller\SaveController &$parentObject);
-
+interface RequestPreProcessInterface
+{
+    /**
+     * Preprocess the request
+     *
+     * @param array $request save request
+     * @param boolean $finished
+     * @param \TYPO3\CMS\FrontendEditing\Controller\SaveController $parentObject
+     * @return array
+     */
+    public function preProcess(
+        array &$request,
+        &$finished,
+        \TYPO3\CMS\FrontendEditing\Controller\SaveController &$parentObject
+    );
 }
-
-?>
