b9523d635c02abb1e068dc2743cd53a9f822efbc
Mattias Nilsson <mattias@pixelant.se>
2016-10-05 13:37:23 +0200

    [TASK] Add UnitTests for ContentEditableWrapper

--- Classes/Utility/ContentEditable/ContentEditableWrapper.php
+++ Classes/Utility/ContentEditable/ContentEditableWrapper.php
@@ -18,9 +18,19 @@ class ContentEditableWrapper
      * @param string $uid
      * @param string $content
      * @return string
+     * @throws \Exception
      */
     public static function wrapContentToBeEditable($table, $field, $uid, $content)
     {
+        // Check that data is not empty
+        if (empty($table)) {
+            throw new \Exception('Property "table" can not to be empty!');
+        } elseif (empty($field)) {
+            throw new \Exception('Property "field" can not to be empty!');
+        } elseif (empty($uid)) {
+            throw new \Exception('Property "uid" can not to be empty!');
+        }
+
         $content = sprintf(
             '<div contenteditable="true" data-table="%s" data-field="%s" data-uid="%s">%s</div>',
             $table,
