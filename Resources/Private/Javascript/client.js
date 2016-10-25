c768245492fc0840960c7cc2796dc92d7d296190
Phat Hoang <phat@pixelant.se>
2016-10-25 13:49:12 +0200

    [TASK] Replace " with '

--- Resources/Private/Javascript/client.js
+++ Resources/Private/Javascript/client.js
@@ -1,7 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Toolbars from "./components/Toolbars"
+import Toolbars from './components/Toolbars'
 
 ReactDOM.render(
     <Toolbars />, 
