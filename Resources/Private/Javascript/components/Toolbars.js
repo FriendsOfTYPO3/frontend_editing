1a9fa9e6938548bf5b7ace7e30b44b736ed67a70
Mattias Nilsson <mattias@pixelant.se>
2016-10-26 08:57:40 +0200

    [TASK] Add webpack for building React

--- Resources/Private/Javascript/components/Toolbars.js
+++ Resources/Private/Javascript/components/Toolbars.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-import TopBar from './Toolbars/TopBar'
+import TopBar from './Toolbars/TopBar';
 
 export default class Toolbars extends React.Component {
     render() {
