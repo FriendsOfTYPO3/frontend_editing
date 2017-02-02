7c528e9f2e1a3211f8dbd66295890ace37c140a9
Mikkel Hastrup <mikkel@pixelant.se>
2017-02-02 12:34:57 +0100

    [TASK] - Changing quick-edit popup window to iframe. Improving spinner and added transition effects.

--- Resources/Public/Javascript/Editor.js
+++ Resources/Public/Javascript/Editor.js
@@ -83,7 +83,7 @@ var Editor = (function($){
 
             // Open/edit action
             that.find('.icon-actions-open').on('click', function() {
-                F.windowOpen(that.data('edit-url'));
+                F.quickEditOpen(that.data('edit-url'));
             });
 
             // Delete action
