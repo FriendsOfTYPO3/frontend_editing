e5c3511a1d3e9c3d72764c6268e1c518980849dd
Mattias Nilsson <mattias@pixelant.se>
2017-06-05 10:37:06 +0200

    [BUGFIX] Remove redundant $requestUrl variable

--- Classes/Hook/FrontendEditingInitializationHook.php
+++ Classes/Hook/FrontendEditingInitializationHook.php
@@ -103,8 +103,6 @@ class FrontendEditingInitializationHook
         $this->typoScriptFrontendController->set_no_cache('Display frontend editing', true);
 
         $requestUrl = GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL');
-
-        $requestUrl = GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL');
         // Check if url has a ?, then decide on URL separator
         if (strpos($requestUrl, '?') !== false) {
             $urlSeparator = '&';
