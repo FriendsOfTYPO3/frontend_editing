dc861b89819eea92c38ff14cc6080f77020ef7fb
Mattias Nilsson <mattias@pixelant.se>
2017-09-06 13:44:47 +0200

    [BUGFIX] Make sure to only display ''Preview mode'' in frontend_editing active
    Fixes: #239

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,31 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['core'][] = 'TYPO3\\CM
 // Disable cHash check when browsing the frontend in frontend editing
 if (TYPO3\CMS\Core\Utility\GeneralUtility::_GET('frontend_editing')) {
     $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFoundOnCHashError'] = false;
+
+    // Add userTsConfig for displaying certain options
+    // when browsing as 'frontend_editing'
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+        # Activate admin panel
+        admPanel {
+            enable.edit = 1
+            enable.preview = 1
+            enable.cache = 0
+            override.edit = 1
+            override.edit.displayFieldIcons = 1
+            override.edit.displayIcons = 0
+            override.preview = 1
+            override.preview.simulateDate = 0
+            override.preview.simulateUserGroup = 0
+            override.preview.showHiddenPages = 1
+            override.preview.showHiddenRecords = 1
+            hide = 1
+        }
+        
+        # Hide "Save and view page" from save options 
+        options.saveDocView = 0
+    ');
 }
+
 /**
  * Hooks
  */
