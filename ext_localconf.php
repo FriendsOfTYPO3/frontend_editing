7a9dfa5c5427ec71ec1ac9f4c865631a3ba3b206
Mattias Nilsson <mattias@pixelant.se>
2017-09-29 13:28:02 +0200

    [TASK] Add a minized set of buttons for TCA saving options
    Fixes: #211
    Fixes: #204

--- ext_localconf.php
+++ ext_localconf.php
@@ -56,6 +56,11 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['cObj
 // Hook to initialize BE user
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/index_ts.php']['postBeUser']['frontend_editing'] =
     \TYPO3\CMS\FrontendEditing\Hook\PostInitializationBEUserHook::class . '->initializeBackendUser';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Template\Components\Buttons\SplitButton::class] = [
+    'className' => \TYPO3\CMS\FrontendEditing\Xclass\TcaButtons::class
+];
+
 /**
  * Pre processors
  */
