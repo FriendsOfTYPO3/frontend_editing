80e35df95fbc4fb94409ea10193ec8e87add074a
Matthias Toscanelli <m.toscanelli@code-source.ch>
2018-02-12 09:05:46 +0100

    [TASK] Code clearing

--- Resources/Public/JavaScript/Editor.js
+++ Resources/Public/JavaScript/Editor.js
@@ -72,123 +72,123 @@ define(['jquery', 'ckeditor', 'ckeditor-jquery-adapter'], function ($, CKEDITOR)
 		// Find all t3-frontend-editing__inline-actions
 		var $inlineActions = $iframeContents.find('span.t3-frontend-editing__inline-actions');
 		var initializeInlineAction = function (index) {
-            var that = $(this);
-            if(!that.data('t3-frontend-editing-initialized')) {
+			var that = $(this);
+			if (!that.data('t3-frontend-editing-initialized')) {
 
-                // Disable dragging icons by mistake
-                that.find('img').on('dragstart', function () {
-                    return false;
-                });
+				// Disable dragging icons by mistake
+				that.find('img').on('dragstart', function () {
+					return false;
+				});
 
-                // Open/edit|new action
-                that.find('.icon-actions-open, .icon-actions-document-new').on('click', function () {
-                    if (!storage.isEmpty()) {
-                        F.confirm(F.translate('notifications.unsaved-changes'), {
-                            yes: function () {
-                                // Do nothing
-                            },
-                            no: function () {
-                                return false;
-                            }
-                        });
-                    } else {
-                        var url = that.data('edit-url');
-                        if ($(this).data('identifier') === 'actions-document-new') {
-                            url = that.data('new-url');
-                        }
-                        require([
-                            'jquery',
-                            'TYPO3/CMS/Backend/Modal',
-                            'TYPO3/CMS/Lang/Lang'
-                        ], function ($, Modal, Lang) {
+				// Open/edit|new action
+				that.find('.icon-actions-open, .icon-actions-document-new').on('click', function () {
+					if (!storage.isEmpty()) {
+						F.confirm(F.translate('notifications.unsaved-changes'), {
+							yes: function () {
+								// Do nothing
+							},
+							no: function () {
+								return false;
+							}
+						});
+					} else {
+						var url = that.data('edit-url');
+						if ($(this).data('identifier') === 'actions-document-new') {
+							url = that.data('new-url');
+						}
+						require([
+							'jquery',
+							'TYPO3/CMS/Backend/Modal',
+							'TYPO3/CMS/Lang/Lang'
+						], function ($, Modal, Lang) {
 
-                            Modal.advanced({
-                                type: Modal.types.iframe,
-                                title: '',
-                                content: url,
-                                size: Modal.sizes.large,
-                                callback: function (currentModal) {
-                                    // Simulate BE environment with correct CKEditor instance for RteLinkBrowser
-                                    currentModal.find(Modal.identifiers.iframe).on('load', function () {
-                                        top.TYPO3.Backend = top.TYPO3.Backend || {};
-                                        top.TYPO3.Backend.ContentContainer = {
-                                            get: function () {
-                                                return currentModal.find(Modal.identifiers.iframe).get(0).contentWindow;
-                                            }
-                                        };
-                                    });
+							Modal.advanced({
+								type: Modal.types.iframe,
+								title: '',
+								content: url,
+								size: Modal.sizes.large,
+								callback: function (currentModal) {
+									// Simulate BE environment with correct CKEditor instance for RteLinkBrowser
+									currentModal.find(Modal.identifiers.iframe).on('load', function () {
+										top.TYPO3.Backend = top.TYPO3.Backend || {};
+										top.TYPO3.Backend.ContentContainer = {
+											get: function () {
+												return currentModal.find(Modal.identifiers.iframe).get(0).contentWindow;
+											}
+										};
+									});
 
-                                    currentModal.on('hidden.bs.modal', function (e) {
-                                        delete top.TYPO3.Backend;
-                                        F.refreshIframe();
-                                    });
-                                }
-                            });
-                        });
-                    }
+									currentModal.on('hidden.bs.modal', function (e) {
+										delete top.TYPO3.Backend;
+										F.refreshIframe();
+									});
+								}
+							});
+						});
+					}
 
-                });
+				});
 
-                // Delete action
-                that.find('.icon-actions-edit-delete').on('click', function () {
-                    F.confirm(F.translate('notifications.delete-content-element'), {
-                        yes: function () {
-                            F.delete(that.data('uid'), that.data('table'));
-                        },
-                        no: function () {
-                            // Do nothing
-                        }
-                    });
-                });
+				// Delete action
+				that.find('.icon-actions-edit-delete').on('click', function () {
+					F.confirm(F.translate('notifications.delete-content-element'), {
+						yes: function () {
+							F.delete(that.data('uid'), that.data('table'));
+						},
+						no: function () {
+							// Do nothing
+						}
+					});
+				});
 
-                // Hide/Unhide action
-                that.find('.icon-actions-edit-hide, .icon-actions-edit-unhide').on('click', function () {
-                    var hide = 1;
-                    if (that.data('hidden') == 1) {
-                        hide = 0;
-                    }
-                    F.hideContent(that.data('uid'), that.data('table'), hide);
-                });
+				// Hide/Unhide action
+				that.find('.icon-actions-edit-hide, .icon-actions-edit-unhide').on('click', function () {
+					var hide = 1;
+					if (that.data('hidden') == 1) {
+						hide = 0;
+					}
+					F.hideContent(that.data('uid'), that.data('table'), hide);
+				});
 
-                if (typeof $inlineActions[index - 1] !== 'undefined' &&
-                    $inlineActions[index - 1].dataset.cid == that.data('cid')
-                ) {
-                    // Move up action
-                    that.find('.icon-actions-move-up').on('click', function () {
-                        // Find the previous editor instance element
-                        var previousDomElementUid = $inlineActions[index - 1].dataset.uid;
-                        var currentDomElementUid = that.data('uid');
-                        var currentDomElementTable = that.data('table');
-                        F.moveContent(previousDomElementUid, currentDomElementTable, currentDomElementUid)
-                    });
-                } else {
-                    that.find('.icon-actions-move-up').hide();
-                }
+				if (typeof $inlineActions[index - 1] !== 'undefined' &&
+					$inlineActions[index - 1].dataset.cid == that.data('cid')
+				) {
+					// Move up action
+					that.find('.icon-actions-move-up').on('click', function () {
+						// Find the previous editor instance element
+						var previousDomElementUid = $inlineActions[index - 1].dataset.uid;
+						var currentDomElementUid = that.data('uid');
+						var currentDomElementTable = that.data('table');
+						F.moveContent(previousDomElementUid, currentDomElementTable, currentDomElementUid)
+					});
+				} else {
+					that.find('.icon-actions-move-up').hide();
+				}
 
-                if (typeof $inlineActions[index + 1] !== 'undefined' &&
-                    $inlineActions[index + 1].dataset.cid == that.data('cid')
-                ) {
-                    // Move down action
-                    that.find('.icon-actions-move-down').on('click', function () {
-                        // Find the next editor instance element
-                        var nextDomElementUid = $inlineActions[index + 1].dataset.uid;
-                        var currentDomElementUid = that.data('uid');
-                        var currentDomElementTable = that.data('table');
-                        F.moveContent(currentDomElementUid, currentDomElementTable, nextDomElementUid)
-                    });
-                } else {
-                    that.find('.icon-actions-move-down').hide();
-                }
-                that.data('t3-frontend-editing-initialized', true);
-            }
-        };
+				if (typeof $inlineActions[index + 1] !== 'undefined' &&
+					$inlineActions[index + 1].dataset.cid == that.data('cid')
+				) {
+					// Move down action
+					that.find('.icon-actions-move-down').on('click', function () {
+						// Find the next editor instance element
+						var nextDomElementUid = $inlineActions[index + 1].dataset.uid;
+						var currentDomElementUid = that.data('uid');
+						var currentDomElementTable = that.data('table');
+						F.moveContent(currentDomElementUid, currentDomElementTable, nextDomElementUid)
+					});
+				} else {
+					that.find('.icon-actions-move-down').hide();
+				}
+				that.data('t3-frontend-editing-initialized', true);
+			}
+		};
 		$inlineActions.each(initializeInlineAction);
 		// Make sure that elements inserted after page initialization are initialized.
-        $iframeContents.on('mouseover', 'span.t3-frontend-editing__inline-actions', function(){
-            //TODO: Find a clean solution to update move up/move down action
+		$iframeContents.on('mouseover', 'span.t3-frontend-editing__inline-actions', function(){
+			// TODO: Find a clean solution to update move up/move down action
 			// Index is set to -10 to make sure both action are hidden
-            initializeInlineAction.call(this, [-10]);
-        });
+			initializeInlineAction.call(this, [-10]);
+		});
 
 
 		var $topBar = $('.t3-frontend-editing__ckeditor-bar');
