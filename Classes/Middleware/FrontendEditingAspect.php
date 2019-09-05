367e83092ca2933325e8c59c3395ca603afbd56e
Mats Svensson <mats@pixelant.se>
2019-09-05 17:56:49 +0200

    [BUGFIX] Use middleware to set visibility aspect and displayFieldEditIcons
    This fixes #349 and fixes #353

---
+++ Classes/Middleware/FrontendEditingAspect.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\FrontendEditing\Middleware;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Exception;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Context\VisibilityAspect;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\FrontendEditing\Utility\FrontendEditingUtility;
+
+/**
+ * This middleware checks if frontend_editing is enabled
+ */
+class FrontendEditingAspect implements MiddlewareInterface
+{
+    /**
+     * Dispatches the request to the corresponding eID class or eID script
+     *
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     * @throws Exception
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (FrontendEditingUtility::isEnabled()) {
+            $showHiddenItems = GeneralUtility::_GET('show_hidden_items') ?? false;
+            $context = GeneralUtility::makeInstance(Context::class);
+            $context->setAspect(
+                'visibility',
+                GeneralUtility::makeInstance(VisibilityAspect::class, $showHiddenItems, $showHiddenItems)
+            );
+        }
+        return $handler->handle($request);
+    }
+}
