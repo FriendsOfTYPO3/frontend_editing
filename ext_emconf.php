2a61e58d14958992bd64c8a3910b50af8b0b85a2
mabolek <mathias@pixelant.no>
2020-04-26 11:03:09 +0200

    [TASK] Set compatibility to include TYPO3 v10 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.9.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.2.99',
-            'rte_ckeditor' => '8.7.0-10.2.99'
+            'typo3' => '8.7.0-10.4.99',
+            'rte_ckeditor' => '8.7.0-10.4.99'
         ],
         'conflicts' => [
             'feedit' => '',
