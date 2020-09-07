032c18a9abcc6c8c95138527fd29ad65f7182ebf
Mathias Bolt Lesniak <mathias@pixelant.no>
2020-09-07 12:34:55 +0200

    [BUGFIX] Prevent inception on form submit (#425)
    * [BUGFIX] LanguageService compatibility for TYPO3 v10
    
    * [TASK] Added check for valid backend user instance
    
    * [BUGFIX] Prevent inception on form submit
    
    * [TASK] Run addFrontendEditingInUrl also on empty query string
    
    * [BUGFIX] Initialize editor also when the page reloads iteself
    
    E.g. through a form submit
    
    * [TASK] Removed unused $.Deferred() definition
    
    * [BUGFIX] Ensure loadingScreenLevel can't go below zero
    
    * [BUGFIX] Display correct URL and page title on form submit

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -42,3 +42,51 @@ config.tx_extbase{
 lib.parseFunc_RTE.tags.a >
 
 [global]
+
+[backend.user.isLoggedIn]
+
+    config.tx_frontendediting {
+        # These transformations are applied to the page being edited to ensure features work as expected and inceptions
+        # are avoided.
+        pageContentTransformations {
+            parseFunc {
+                tags {
+                    form = TEXT
+                    form {
+                        current = 1
+
+                        # Add frontend_editing=true if this is a GET form (rather than POST)
+                        innerWrap = <input type="hidden" name="frontend_editing" value="true">|
+                        innerWrap.if {
+                            value.data = parameters : method
+                            value.case = lower
+                            equals = get
+                        }
+
+                        dataWrap = <form { parameters : allParams }>|</form>
+                    }
+                }
+            }
+
+            HTMLparser = 1
+            HTMLparser {
+                keepNonMatchedTags = 1
+
+                tags {
+                    a.fixAttrib {
+                        href.userFunc = TYPO3\CMS\FrontendEditing\UserFunc\HtmlParserUserFunc->removeFrontendEditingInUrl
+
+                        target.list = _self
+                    }
+
+                    form.fixAttrib {
+                        action.userFunc = TYPO3\CMS\FrontendEditing\UserFunc\HtmlParserUserFunc->addFrontendEditingInUrl
+
+                        target.list = _self
+                    }
+                }
+            }
+        }
+    }
+
+[global]
