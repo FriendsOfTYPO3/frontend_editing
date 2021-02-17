3d99393655fbb88e4eb847d3a8c2433bb0b8f0fc
Jan Messer <jan@mtec-studios.ch>
2021-02-17 23:23:55 +0100

    imported backend.css file, bugfix scrollarea-arrow css width mix (rem and px) made scrollarea-arrow border smaller

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,1 +1,9 @@
+import '../../Resources/Public/Css/frontend_editing.css';
 import '../../Resources/Public/Css/inline_editing.css';
+
+try {
+    require('../../.Build/Web/typo3/sysext/backend/Resources/Public/Css/backend.css');
+} catch (ex) {
+    // shit happens
+    console.log(ex);
+}
