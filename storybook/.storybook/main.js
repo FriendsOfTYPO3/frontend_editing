8e21b1b9e7d8f8d20d23a0fc3bb3f7cacc8241e1
Jan Messer <jan@mtec-studios.ch>
2021-02-19 00:02:33 +0100

    fixed missing story mdx file extension

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     "stories": [
-        "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+        "../stories/**/*.stories.@(mdx|js|jsx|ts|tsx)"
     ],
     "addons": [
         "@storybook/addon-links",
