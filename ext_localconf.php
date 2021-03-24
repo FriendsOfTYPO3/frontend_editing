88003099b1841d5041b866eefa25288809cfee55
Mattias Nilsson <tollepjaer@gmail.com>
2021-03-24 13:54:53 +0100

    [TASK] Append parameter (frontend_editing_enabled) to all typolinks in backend when using frontend editing

--- ext_localconf.php
+++ ext_localconf.php
@@ -29,6 +29,10 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['cObj
     \TYPO3\CMS\FrontendEditing\Hook\ContentObjectRendererHook::class
 ];
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typolinkProcessing']['typolinkModifyParameterForPageLinks'][] =
+    \TYPO3\CMS\FrontendEditing\Hook\ModifyTypoLinkConfig::class;
+
+
 /**
  * Pre processors
  */
