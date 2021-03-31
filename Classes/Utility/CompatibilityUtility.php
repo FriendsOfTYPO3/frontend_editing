dcae20ebf7f938756af36b5fa1cec6449b95e6b4
Mattias Nilsson <tollepjaer@gmail.com>
2021-03-31 09:49:37 +0200

    [TASK] Fix broken rendering for TYPO3 9
    Fixes: #516

---
+++ Classes/Utility/CompatibilityUtility.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types = 1);
+
+namespace TYPO3\CMS\FrontendEditing\Utility;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+/**
+ * Miscellaneous functions relating to compatibility with different TYPO3 versions
+ */
+class CompatibilityUtility
+{
+    /**
+     * Returns true if the current TYPO3 version is less than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThan($version)
+    {
+        return self::getTypo3VersionInteger() < VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is less than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() <= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThan($version)
+    {
+        return self::getTypo3VersionInteger() > VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() >= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns the TYPO3 version as an integer
+     *
+     * @return int
+     */
+    public static function getTypo3VersionInteger()
+    {
+        return VersionNumberUtility::convertVersionNumberToInteger(VersionNumberUtility::getNumericTypo3Version());
+    }
+}
