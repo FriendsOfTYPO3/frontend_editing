9ea4028396e5ed7e89e2a188d87f38ef13729a11
mabolek <mathias@lilio.com>
2021-04-07 17:06:37 +0200

    [TASK][!!!] Processes incoming values using stdWrap
    Removes the old RequestPreProcess hook.
    
    Two TypoScript configuration arrays are used (in order of preference):
    
    `config.tx_frontendediting.contentPersistPreProcessing` contains type and field-specific configurations.
    `contentPersistPreProcessing.<table>.<type>.<field>` accepts any stdWrap configuratoion property.
    Setting `<type>` to "*" indicates a wildcard catching all types not explicitly set.
    `<type>` "0" (zero as string) is the default type of any TYPO3 record.
    
    config.tx_frontendediting.contentPersistPreProcessingPatterns` will be used if no configuration matches are
    found in `contentPersistPreProcessing`.
    `contentPersistPreProcessingPatterns.<rte-preset>` accepts any stdWrap configuratoion property.
    `<rte-preset>` is any RTE preset string, as set in a field's TCA configuration `frontendConfiguration` or
    `frontendRichtextConfiguration`.

---
+++ Classes/Controller/Event/PrepareFieldUpdateEventHandlerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace TYPO3\CMS\FrontendEditing\Controller\Event;
+
+/**
+ * Interface for handling PrepareFieldUpdate events.
+ */
+interface PrepareFieldUpdateEventHandlerInterface
+{
+    /**
+     * Handle a PrepareFieldUpdateEvent.
+     *
+     * @param PrepareFieldUpdateEvent $event
+     */
+    public function __invoke(PrepareFieldUpdateEvent $event): void;
+}
