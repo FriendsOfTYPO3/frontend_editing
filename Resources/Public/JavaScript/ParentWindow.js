e4344ee8ab43cac2af06b097d73ade5154ee151f
Mattias Nilsson <tollepjaer@gmail.com>
2023-03-28 18:29:12 +0200

    [BUGFIX] Accessing the frame using .contentWindow does not work in Chrome or Edge. (#649)

--- Resources/Public/JavaScript/ParentWindow.js
+++ Resources/Public/JavaScript/ParentWindow.js
@@ -1,26 +1,31 @@
 define([
-    'TYPO3/CMS/Backend/FormEngineLinkBrowserAdapter',
-    'TYPO3/CMS/Recordlist/ElementBrowser'
-  ], function(FormEngineLinkBrowserAdapter, ElementBrowser) {
-    const FormEngineLinkBrowserAdapterParentFunction = FormEngineLinkBrowserAdapter.getParent;
-    const getParent = () => {
-      if (
-        typeof window.parent !== 'undefined' &&
-        typeof window.parent.document.list_frame !== 'undefined' &&
-        window.parent.document.list_frame.length > 0 &&
-        window.parent.document.list_frame[window.parent.document.list_frame.length - 1].contentWindow.parent.document.querySelector('.t3js-modal-iframe') !== null
-      ) {
-        return window.parent.document.list_frame[window.parent.document.list_frame.length - 1].contentWindow;
+  'TYPO3/CMS/Backend/FormEngineLinkBrowserAdapter',
+  'TYPO3/CMS/Recordlist/ElementBrowser'
+], function(FormEngineLinkBrowserAdapter, ElementBrowser) {
+  const FormEngineLinkBrowserAdapterParentFunction = FormEngineLinkBrowserAdapter.getParent;
+  const getParent = () => {
+    // Accessing the frame using .contentWindow does not
+    // work in Chrome or Edge.
+    if (
+      typeof window.parent !== 'undefined' &&
+      typeof window.parent.document.list_frame !== 'undefined' &&
+      window.parent.document.list_frame.length > 0
+    ) {
+      var frame = window.parent.document.list_frame[window.parent.document.list_frame.length - 1];
+      frame = frame.contentWindow || frame;
+      if (frame.parent.document.querySelector('.t3js-modal-iframe') !== null) {
+        return frame;
       }
-      return null;
     }
+    return null;
+  }
 
-    FormEngineLinkBrowserAdapter.getParent = () => {
-      return getParent() || FormEngineLinkBrowserAdapterParentFunction();
-    }
+  FormEngineLinkBrowserAdapter.getParent = () => {
+    return getParent() || FormEngineLinkBrowserAdapterParentFunction();
+  }
 
-    ElementBrowser.getParent = () => {
-      ElementBrowser.opener = FormEngineLinkBrowserAdapter.getParent()
-        return ElementBrowser.opener;
-    }
+  ElementBrowser.getParent = () => {
+    ElementBrowser.opener = FormEngineLinkBrowserAdapter.getParent()
+      return ElementBrowser.opener;
+  }
 });
