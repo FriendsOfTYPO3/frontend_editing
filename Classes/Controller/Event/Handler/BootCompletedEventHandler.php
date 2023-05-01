77fdc76fce842dc8f35816fc7fdab4ef94c6719c
Mattias Nilsson <tollepjaer@gmail.com>
2023-05-01 18:54:13 +0200

    [BUGFIX] Add check for REQUEST_URI (#658)
    Fixes: #657

--- Classes/Controller/Event/Handler/BootCompletedEventHandler.php
+++ Classes/Controller/Event/Handler/BootCompletedEventHandler.php
@@ -7,15 +7,20 @@ namespace TYPO3\CMS\FrontendEditing\Controller\Event\Handler;
 use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
 use TYPO3\CMS\Core\Page\PageRenderer;
 
+/**
+ * Event Handler Boot loading complete.
+ */
 final class BootCompletedEventHandler
 {
     public function __invoke(BootCompletedEvent $event): void
     {
-        $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(PageRenderer::class);
-        $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(PageRenderer::class);
+            $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-        if (str_ends_with($path, '/browse')) {
-            $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+            if (str_ends_with($path, '/browse')) {
+                $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+            }
         }
     }
 }
