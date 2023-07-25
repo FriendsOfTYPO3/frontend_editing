be26b79929c179b49daac8524d0d45449e2ea849
Christian Toffolo <ian@webian.it>
2023-07-25 13:43:22 +0200

    [BUGFIX] Strengthen request URI check (#666)
    * [TASK] Instantiate PageRenderer only if really needed
    
    * [BUGFIX] Strengthen request URI check
    
    Crawlers and bots sometimes make request with malformed URI. E.g.:
    - https://example.com//example.json" (notice the double slash before example.json)
    - https://example.com/:443/favicon.ico
    
    This is to avoid exception in these cases.
    
    * [TASK] Add comment

--- Classes/Controller/Event/Handler/BootCompletedEventHandler.php
+++ Classes/Controller/Event/Handler/BootCompletedEventHandler.php
@@ -6,6 +6,7 @@ namespace TYPO3\CMS\FrontendEditing\Controller\Event\Handler;
 
 use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Event Handler Boot loading complete.
@@ -14,12 +15,16 @@ final class BootCompletedEventHandler
 {
     public function __invoke(BootCompletedEvent $event): void
     {
-        if (isset($_SERVER['REQUEST_URI'])) {
-            $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(PageRenderer::class);
-            $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
-            if (str_ends_with($path, '/browse')) {
-                $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+        // Load JS ParentWindow when user opens the element browser (file selector)
+        $requestUri = GeneralUtility::getIndpEnv('REQUEST_URI');
+        if ($requestUri !== '') {
+            $phpUrlPath = parse_url($requestUri, PHP_URL_PATH);
+            if ($phpUrlPath) {
+                $path = urldecode($phpUrlPath);
+                if (str_ends_with($path, '/browse')) {
+                    $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+                    $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+                }
             }
         }
     }
