76b21549419d3ec56779029cdd6cf52adb72091d
Christian Toffolo <ian@webian.it>
2023-07-25 12:28:33 +0200

    [TASK] Instantiate PageRenderer only if really needed

--- Classes/Controller/Event/Handler/BootCompletedEventHandler.php
+++ Classes/Controller/Event/Handler/BootCompletedEventHandler.php
@@ -6,6 +6,7 @@ namespace TYPO3\CMS\FrontendEditing\Controller\Event\Handler;
 
 use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Event Handler Boot loading complete.
@@ -15,10 +16,10 @@ final class BootCompletedEventHandler
     public function __invoke(BootCompletedEvent $event): void
     {
         if (isset($_SERVER['REQUEST_URI'])) {
-            $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(PageRenderer::class);
             $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
             if (str_ends_with($path, '/browse')) {
+                $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
                 $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
             }
         }
