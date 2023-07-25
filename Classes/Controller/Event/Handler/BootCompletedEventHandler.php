9b1d91bbc067581669a477d8f8ba8b52cf34548c
Christian Toffolo <ian@webian.it>
2023-07-25 10:42:50 +0200

    [BUGFIX] Strengthen request URI check
    This is to avoid exception in case of requests URI like https://example.com//example.json" (notice the double slash before example.json)

--- Classes/Controller/Event/Handler/BootCompletedEventHandler.php
+++ Classes/Controller/Event/Handler/BootCompletedEventHandler.php
@@ -6,6 +6,7 @@ namespace TYPO3\CMS\FrontendEditing\Controller\Event\Handler;
 
 use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Event Handler Boot loading complete.
@@ -14,9 +15,10 @@ final class BootCompletedEventHandler
 {
     public function __invoke(BootCompletedEvent $event): void
     {
-        if (isset($_SERVER['REQUEST_URI'])) {
-            $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(PageRenderer::class);
-            $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        $requestUri = GeneralUtility::getIndpEnv('REQUEST_URI');
+        if ($requestUri !== '') {
+            $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+            $path = urldecode(parse_url($requestUri, PHP_URL_PATH));
 
             if (str_ends_with($path, '/browse')) {
                 $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
