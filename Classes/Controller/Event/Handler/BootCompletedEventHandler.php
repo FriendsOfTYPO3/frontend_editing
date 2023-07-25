7cee1f236c608d5bf7922fe1d5f50b73bb0fb328
Christian Toffolo <ian@webian.it>
2023-07-25 12:42:58 +0200

    [BUGFIX] Strengthen request URI check
    Crawlers and bots sometimes make request with malformed URI. E.g.:
    - https://example.com//example.json" (notice the double slash before example.json)
    - https://example.com/:443/favicon.ico
    
    This is to avoid exception in these cases.

--- Classes/Controller/Event/Handler/BootCompletedEventHandler.php
+++ Classes/Controller/Event/Handler/BootCompletedEventHandler.php
@@ -15,12 +15,15 @@ final class BootCompletedEventHandler
 {
     public function __invoke(BootCompletedEvent $event): void
     {
-        if (isset($_SERVER['REQUEST_URI'])) {
-            $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
-            if (str_ends_with($path, '/browse')) {
-                $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-                $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+        $requestUri = GeneralUtility::getIndpEnv('REQUEST_URI');
+        if ($requestUri !== '') {
+            $phpUrlPath = parse_url($requestUri, PHP_URL_PATH);
+            if ($phpUrlPath) {
+                $path = urldecode($phpUrlPath);
+                if (str_ends_with($path, '/browse')) {
+                    $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+                    $pageRenderer->loadRequireJsModule('TYPO3/CMS/FrontendEditing/ParentWindow');
+                }
             }
         }
     }
